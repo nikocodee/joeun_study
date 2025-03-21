83228e44d5a3895abf116ed25da96d33c99c041e
kimi <kjki93@naver.com>
2025-03-21 18:17:20 +0900

    0321 feat: Test integration of FastAPI server with React and add multipart code

--- Workdir/java/workplace_sts_4.22/study-12/src/main/java/com/learn/controller/BoardController.java
+++ Workdir/java/workplace_sts_4.22/study-12/src/main/java/com/learn/controller/BoardController.java
@@ -1,23 +1,34 @@
 package com.learn.controller;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/board")
 public class BoardController {
 
+	 private final Path uploadPath = Path.of(System.getProperty("user.dir"), "uploads");
+	 
 	// get방식은 query string으로 받음
 	@GetMapping
 	public Map<String, Object> getBoards(@RequestParam Map<String, Object> params) {
+		log.info("*** getBoards Call! ***");
 		Map<String, Object> result = new HashMap<>();
 		List<Map<String, Object>> content = new ArrayList<>();
 
@@ -25,20 +36,20 @@ public class BoardController {
 //		map1.put("content", params.get("searchKeyWord"));
 //		map1.put("writer", params.get("name"));
 //		map1.put("title", params.get("title"));
-		map1.put("content", "test content1 ");
-		map1.put("writer", "Writer name");
-		map1.put("title", "title test ");
+		map1.put("content", "test Content1 ");
+		map1.put("writer", "test Writer1");
+		map1.put("title", "test Title1");
 		map1.put("id", 1);
 
 		Map<String, Object> map2 = new HashMap<>();
-		map2.put("content", "test content2");
-		map2.put("writer", "Writer name2");
-		map2.put("title", "title test2");
+		map2.put("content", "test Content2 ");
+		map2.put("writer", "test Writer2");
+		map2.put("title", "test Title2");
 		map2.put("id", 2);
 
 		content.add(map1);
 		content.add(map2);
-		
+
 		result.put("content", content);
 		result.put("totalPages", 10);
 
@@ -49,12 +60,51 @@ public class BoardController {
 	public Map<String, Object> getBoardById(@PathVariable long id) {
 		Map<String, Object> result = new HashMap<>();
 		result.put("content", "content text");
-		result.put("writer", "writer test");
-		result.put("title", "test title" + id);
+		result.put("writer", "writer kim");
+		result.put("title", "test title " + id);
 		result.put("createdAt", "2025/05/05");
 		result.put("views", 13);
 		result.put("id", id);
 		return result;
+	}
+
+	public Map<String, Object> createBoard(MultipartHttpServletRequest req) {
+		Map<String, Object> result = new HashMap<>();
+		List<MultipartFile> files = new ArrayList<>();
+		for (int i = 0; i < req.getFiles("file").size(); i++) {
+			files.add(req.getFiles("file").get(i));
+		}
+		boolean cond1 = files != null;
+		boolean cond2 = files.size() > 0;
+		if (cond1 && cond2) {
+			//file은 원소, files는 list
+			for(MultipartFile file : files) {
+				String randomUUID = UUID.randomUUID().toString();
+				String originalFileName = file.getOriginalFilename();
+				result.put(originalFileName, randomUUID);
+				String Extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+				log.debug(randomUUID, "|", originalFileName, "|", Extension);
+				String saveFileName = randomUUID + Extension;
+				
+				Path target = uploadPath.resolve(saveFileName);
+				try {
+					//target에 파일 업로드
+					file.transferTo(target);
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+//					e.printStackTrace(); //운영때는 하면 안됨
+					log.error("createBoard IllegalStateException raise!!!"+e.getMessage());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+//					e.printStackTrace();
+					log.error("createBoard IOException raise!!!"+e.getMessage());
+				} catch (Exception e) {
+//					e.printStackTrace();
+					log.error("createBoard Exception raise!!!"+e.getMessage());
+				}
+			}
+		}
+			return result;
 	}
 }
 
@@ -105,4 +155,3 @@ public class BoardController {
 //      return result;
 //   }
 //}
-
--- Workdir/python/20250320/main.py
+++ Workdir/python/20250320/main.py
@@ -1,26 +1,35 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # 서버 객체 생성
 app = FastAPI()
 
-# 1. GET 요청: 기본 데이터를 조회하는 API
+# CORS 미들웨어 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # 모든 도메인 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
+
+# 1. GET 요청: 전체 게시물 목록 조회하는 API
+@app.get("/board")
+def getBoards():
+    content = [{"writer":"Writer name","id":1,"title":"title test ","content":"test content1 "},{"writer":"Writer name2","id":2,"title":"title test2","content":"test content2"}]
+    return {"content": content, "totalPages": 100}
+
+# 2. GET 요청: 게시물 상세페이지 조회하는 API
 # path variable 필수 요소에만 사용
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": int(q)}
-
-# 2. POST 요청: 새로운 아이템을 추가하는 API (JSON으로 보냄)
-@app.post("/items/")
-def create_item(name: str, description: str = None):
-    return {"name": name, "description": description}
-
-# 3. PUT 요청: 기존 아이템을 수정하는 API
-@app.put("/items/{item_id}")
-def update_item(item_id: int, name: str):
-    return {"item_id": item_id, "name": name}
-
-# 4. DELETE 요청: 특정 아이템을 삭제하는 API
-@app.delete("/items/{item_id}")
-def delete_item(item_id: int):
-    return {"message": f"Item {item_id} deleted"}
+@app.get("/board/{id}")
+def getBoardById(id: int):
+    content = {
+    "createdAt": "2025/05/05",
+    "writer": "writer test",
+    "id": id,
+    "title": f"test title {id}",
+    "content": "content text",
+    "views": 13
+}
+    return content
