017c69aeee1f508d2ed63894bc34b35db6cdbecf
kimi <kjki93@naver.com>
2025-05-21 17:24:23 +0900

    0521 feat: Implement product CRUD in Springboot

--- Workdir/study/backend/mallapi/src/main/java/org/zerock/mallapi/controller/ProductController.java
+++ Workdir/study/backend/mallapi/src/main/java/org/zerock/mallapi/controller/ProductController.java
@@ -2,12 +2,15 @@ package org.zerock.mallapi.controller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -38,7 +41,7 @@ public class ProductController {
     }
 
     @PostMapping("/")
-    public Map<String, String> register(ProductDTO productDTO){
+    public Map<String, Long> register(ProductDTO productDTO){
         log.info("register: " + productDTO);
 
         List<MultipartFile> files = productDTO.getFiles();
@@ -49,7 +52,11 @@ public class ProductController {
 
         log.info(uploadFileNames);
 
-        return Map.of("RESULT", "SUCCESS");
+        Long pno = productService.register(productDTO);
+
+        return Map.of("result", pno);
+
+        // return Map.of("RESULT", "SUCCESS");
     }
 
     @GetMapping("/view/{fileName}")
@@ -57,4 +64,63 @@ public class ProductController {
         
         return fileUtil.getFile(fileName);
     }
+
+    @GetMapping("/{pno}")
+    public ProductDTO read(@PathVariable(name = "pno") Long pno){
+        return productService.get(pno);
+    }
+
+    @PutMapping("/{pno}")
+    public Map<String, String> modify(@PathVariable(name="pno") Long pno, ProductDTO productDTO){
+
+        productDTO.setPno(pno);
+
+        ProductDTO oldProductDTO = productService.get(pno);
+
+        //기존 파일들(DB에 존재하는 파일들)
+        List<String> oldFileNames = oldProductDTO.getUploadFileNames();
+
+        //새로 업로드 해야하는 파일들
+        List<MultipartFile> files = productDTO.getFiles();
+
+        //새로 업로드되어서 만들어진 파일 이름들
+        List<String> currentUploadFileNames = fileUtil.saveFiles(files);
+
+        //화면에서 변화 없이 계속 유지된 파일들
+        List<String> uploadFileNames = productDTO.getUploadFileNames();
+
+        //유지되는 파일들 + 새로 업로드된 파일 이름들이 저장해야 하는 파일 목록이 됨
+        if(currentUploadFileNames != null && currentUploadFileNames.size() > 0) {
+            uploadFileNames.addAll(currentUploadFileNames);
+        }
+
+        //수정 작업
+        productService.modify(productDTO);
+
+        if(oldFileNames != null && oldFileNames.size() > 0) {
+            //지워야 하는 파일 목록 찾기
+            //예전 파일들 중에서 지워져야 하는 파일이름들
+            List<String> removeFiles = oldFileNames
+            .stream()
+            .filter(fileName -> uploadFileNames.indexOf(fileName) == -1).collect(Collectors.toList());
+
+            //실제 파일 삭제
+            fileUtil.deleteFiles(removeFiles);
+        }
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("/{pno}")
+    public Map<String, String> remove(@PathVariable("pno") Long pno){
+        
+        //삭제해야할 파일들 알아내기
+        List<String> oldFileNames = productService.get(pno).getUploadFileNames();
+
+        productService.remove(pno);
+
+        fileUtil.deleteFiles(oldFileNames);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+
 }
--- Workdir/study/backend/mallapi/src/main/java/org/zerock/mallapi/service/ProductService.java
+++ Workdir/study/backend/mallapi/src/main/java/org/zerock/mallapi/service/ProductService.java
@@ -9,5 +9,13 @@ import jakarta.transaction.Transactional;
 @Transactional
 public interface ProductService {
     PageResponseDTO<ProductDTO> getList(PageRequestDTO pageRequestDTO);
+
+    Long register(ProductDTO productDTO);
+
+    ProductDTO get(Long pno);
+
+    void modify(ProductDTO productDTO);
+
+    void remove(Long pno);
 }
  
